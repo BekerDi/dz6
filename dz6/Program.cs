30f13c1f3763d8a7e2ad3efb5083c52069346ce8
BekerDi <d.beker.04@mail.ru>
2023-10-18 20:55:05 +0400

    Добавьте файлы проекта.

---
+++ dz6/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System;
+
+public enum AccountType
+{
+    Checking,
+    Savings
+}
+
+public class BankAccount
+{
+    private static int accountNumberCounter;
+
+    public int AccountNumber { get; }
+    public decimal Balance { get; private set; }
+    public AccountType Type { get; }
+
+    public BankAccount(AccountType type)
+    {
+        AccountNumber = GenerateAccountNumber();
+        Balance = 0;
+        Type = type;
+    }
+
+    private static int GenerateAccountNumber()
+    {
+        return ++accountNumberCounter;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+        Console.WriteLine($"Текущий баланс: {amount} нa {AccountNumber}.");
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Снято {amount} со счета {AccountNumber}. Новый баланс: {Balance}");
+        }
+        else
+        {
+            Console.WriteLine($"Недостаточно средств на счете {AccountNumber}. Невозможно снять {amount}");
+        }
+    }
+
+    public void DepositFromUser()
+    {
+        Console.Write($"Введите сумму для внесения на счет {AccountNumber}: ");
+        decimal depositAmount;
+
+        while (!decimal.TryParse(Console.ReadLine(), out depositAmount))
+        {
+            Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+            Console.Write($"Введите сумму для внесения на счет {AccountNumber}: ");
+        }
+
+        Deposit(depositAmount);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BankAccount account1 = new BankAccount(AccountType.Checking);
+        BankAccount account2 = new BankAccount(AccountType.Savings);
+
+        account1.Deposit(10000000);
+
+        Console.Write("Введите сумму для снятия со счета 1: ");
+        decimal withdrawalAmount1;
+
+        while (!decimal.TryParse(Console.ReadLine(), out withdrawalAmount1))
+        {
+            Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+            Console.Write("Введите сумму для снятия со счета 1: ");
+        }
+
+        account1.Withdraw(withdrawalAmount1);
+
+        account2.Deposit(2000000);
+
+        Console.Write("Введите сумму для снятия со счета 2: ");
+        decimal withdrawalAmount2;
+
+        while (!decimal.TryParse(Console.ReadLine(), out withdrawalAmount2))
+        {
+            Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+            Console.Write("Введите сумму для снятия со счета 2: ");
+        }
+
+        account2.Withdraw(withdrawalAmount2);
+
+        account1.DepositFromUser();
+        account2.DepositFromUser();
+        Console.ReadKey();
+    }
+}
+
+
---
+++ tumakov7/Program.cs
@@ -0,0 +1,199 @@
+﻿
+
+
+using System;
+
+namespace CasinoInterface
+{
+    // Абстрактный класс "Игра"
+    public abstract class Game
+    {
+        public string Name { get; }
+        public int MinBet { get; }
+        public int MaxBet { get; }
+
+        public Game(string name, int minBet, int maxBet)
+        {
+            Name = name;
+            MinBet = minBet;
+            MaxBet = maxBet;
+        }
+
+        // Абстрактные методы
+        public abstract void Play();
+        public abstract void Bet(int amount);
+        public abstract void Win(int amount);
+        public abstract void Lose(int amount);
+        public abstract void Quit();
+    }
+
+    // Класс "Слот-машина"
+    public class SlotMachine : Game
+    {
+        public int NumOfReels { get; }
+        public string Theme { get; }
+
+        public SlotMachine(string name, int minBet, int maxBet, int numOfReels, string theme) : base(name, minBet, maxBet)
+        {
+            NumOfReels = numOfReels;
+            Theme = theme;
+        }
+
+        // Методы для слот-машины
+        public override void Play()
+        {
+            Console.WriteLine("Играем на слот-машине");
+        }
+
+        public override void Bet(int amount)
+        {
+            Console.WriteLine($"Ставим {amount} на слот-машину");
+        }
+
+        public override void Win(int amount)
+        {
+            Console.WriteLine($"Выигрыш: {amount}");
+        }
+
+        public override void Lose( int amount)
+        {
+            Console.WriteLine($"Проигрыш: {amount} ");
+        }
+
+        public override void Quit()
+        {
+            Console.WriteLine("Выход из игры на слот-машине");
+        }
+    }
+
+    // Класс "Покер"
+    public class Poker : Game
+    {
+        public int NumOfPlayers { get; }
+        public string DeckType { get; set; }
+
+        public Poker(string name, int minBet, int maxBet, int numOfPlayers, string deckType) : base(name, minBet, maxBet)
+        {
+            NumOfPlayers = numOfPlayers;
+            DeckType = deckType;
+        }
+
+        // Методы для покера
+        public override void Play()
+        {
+            Console.WriteLine("Играем в покер");
+        }
+
+        public override void Bet(int amount)
+        {
+            Console.WriteLine($"Ставим {amount} в покере");
+        }
+
+        public override void Win(int amount)
+        {
+            Console.WriteLine($"Выигрыш: {amount}");
+        }
+
+        public override void Lose(int amount)
+        {
+            Console.WriteLine($"Проигрыш: {amount}");
+        }
+
+        public override void Quit()
+        {
+            Console.WriteLine("Выход из игры в покере");
+        }
+    }
+
+    // Класс "21"
+    public class TwentyOne : Game
+    {
+        public int NumOfDecks { get; }
+        public string Variation { get; }
+
+        public TwentyOne(string name, int minBet, int maxBet, int numOfDecks, string variation) : base(name, minBet, maxBet)
+        {
+            NumOfDecks = numOfDecks;
+            Variation = variation;
+        }
+
+        // Методы для игры "21"
+        public override void Play()
+        {
+            Console.WriteLine("Играем в 21");
+        }
+
+        public override void Bet(int amount)
+        {
+            Console.WriteLine($"Ставим {amount} в 21");
+        }
+
+        public override void Win(int amount)
+        {
+            Console.WriteLine($"Выигрыш: {amount}");
+        }
+
+        public override void Lose(int amount)
+        {
+            Console.WriteLine($"Проигрыш: {amount}");
+        }
+
+        public override void Quit()
+        {
+            Console.WriteLine("Выход из игры в 21");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта слот-машины
+            SlotMachine slotMachine = new SlotMachine("Слот-машина", 1, 10, 3, "Фруктовая");
+            Console.WriteLine($"Название игры: {slotMachine.Name}");
+            Console.WriteLine($"Минимальная ставка: {slotMachine.MinBet}");
+            Console.WriteLine($"Максимальная ставка: {slotMachine.MaxBet}");
+            Console.WriteLine($"Количество барабанов: {slotMachine.NumOfReels}");
+            Console.WriteLine($"Тема: {slotMachine.Theme}");
+            slotMachine.Play();
+            slotMachine.Bet(5);
+            slotMachine.Win(10);
+            slotMachine.Lose(1);
+            slotMachine.Quit();
+
+            Console.WriteLine();
+
+            // Создание объекта покера
+            Poker poker = new Poker("Покер", 10, 100, 4, "Стандартная");
+            Console.WriteLine($"Название игры: {poker.Name}");
+            Console.WriteLine($"Минимальная ставка: {poker.MinBet}");
+            Console.WriteLine($"Максимальная ставка: {poker.MaxBet}");
+            Console.WriteLine($"Количество игроков: {poker.NumOfPlayers}");
+            Console.WriteLine($"Тип колоды: {poker.DeckType}");
+
+            poker.Play();
+            poker.Bet(20);
+            poker.Win(78);
+            poker.Lose(0);
+            poker.Quit();
+
+            Console.WriteLine();
+
+            // Создание объекта "21"
+            TwentyOne twentyOne = new TwentyOne("21", 5, 500, 6, "Классическая");
+            Console.WriteLine($"Название игры: {twentyOne.Name}");
+            Console.WriteLine($"Минимальная ставка: {twentyOne.MinBet}");
+            Console.WriteLine($"Максимальная ставка: {twentyOne.MaxBet}");
+            Console.WriteLine($"Количество колод: {twentyOne.NumOfDecks}");
+            Console.WriteLine($"Вариация: {twentyOne.Variation}");
+
+            twentyOne.Play();
+            twentyOne.Bet(50);
+            twentyOne.Win(100);
+            twentyOne.Lose(15);
+            twentyOne.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
